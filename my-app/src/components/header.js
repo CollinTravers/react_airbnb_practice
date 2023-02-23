db85978103a9112f7c7e04b6dd1c4c54b3ceef05
Collin Travers <collintravers1@gmail.com>
2023-02-22 23:54:59 -0500

    Fixing image paths

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,11 +1,10 @@
 import React from "react"
 import './header.css'
-import logo from "../images/airbnb-logo.png"
 
 function Header () {
   return (
     <nav>
-      <img src={logo} className="nav--logo"/>
+      <img src="../images/airbnb-logo.png" className="nav--logo"/>
     </nav>
   )
 }
--- my-app/src/components/hero.js
+++ my-app/src/components/hero.js
@@ -1,11 +1,10 @@
 import React from "react"
-import logo from '../images/photo-grid.png'
 import './hero.css'
 
 function Hero() {
   return(
     <section className="hero--container">
-      <img src={logo} className="hero--photo"/>
+      <img src="../images/photo-grid.png" className="hero--photo"/>
       <h1 className="hero--header">Online Experiences</h1>
       <p className="hero--paragraph">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>  
     </section>
